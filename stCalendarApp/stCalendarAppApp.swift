81ee364fba23f1febfe61518635c106792444a4b
jay-juns <necrozerg2@naver.com>
2024-04-24 18:50:09 +0900

    Initial Commit

---
+++ stCalendarApp/stCalendarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  stCalendarAppApp.swift
+//  stCalendarApp
+//
+//  Created by junseok Kim on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct stCalendarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
